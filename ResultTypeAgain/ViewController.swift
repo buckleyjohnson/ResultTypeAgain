a6c0a207fd51f508e54e31f5bf39e963a4daedb7
Buckley Johnson <buck@qksee.com>
2019-11-08 19:07:16 -0700

    Initial Commit

---
+++ ResultTypeAgain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResultTypeAgain
+//
+//  Created by buckley johnson on 11/8/19.
+//  Copyright © 2019 buckley johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
