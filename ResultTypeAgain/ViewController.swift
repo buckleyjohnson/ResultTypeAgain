e595645b4083c500bd252d36964fa93851b3acb9
Buckley Johnson <buck@qksee.com>
2019-11-09 08:31:45 -0700

    Changed var res to results

--- ResultTypeAgain/ViewController.swift
+++ ResultTypeAgain/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Course: Decodable {
+        let id: Int
+        let name: String
+        let imageUrl: String
+        let number_of_lessons: Int
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fetchCoursesJSON { (results) in
+            switch results {
+            case .success(let courses):
+                courses.forEach({ (course) in
+                    print(course.name)
+                })
+            case .failure(let err):
+                print("failed", err)
+            }
+            
+        }
+
     }
 
 
+    
+    fileprivate func fetchCoursesJSON(completion: @escaping (Result<[Course], Error>) -> ()){
+        let urlString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+        guard let url = URL(string: urlString) else {return}
+        URLSession.shared.dataTask(with:url) {(data, resp, err) in
+            if let err = err {
+                completion(.failure(err))
+                return
+            }
+            
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data!)
+                completion(.success(courses))
+            }catch let jsonError{
+                completion(.failure(jsonError))
+            }
+         
+            
+        }.resume()
+    }
 }
 
